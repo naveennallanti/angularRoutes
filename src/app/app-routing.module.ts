02de2a10623df162115fefce0539e1de74f12789
naveen <naveen.nallanti@wishfin.com>
2019-02-13 17:20:04 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {EmployeeListComponentComponent} from './app/components/employee-list-component/employee-list-component.component';
-import {DepartmentListComponentComponent} from './app/components/department-list-component/department-list-component.component';
+import {EmployeeListComponentComponent} from './components/employee-list-component/employee-list-component.component';
+import {DepartmentListComponentComponent} from './components/department-list-component/department-list-component.component';
+import {DepartmentDetailComponentComponent} from './components/department-detail-component/department-detail-component.component';
+import {HomeComponent} from './components/home/home.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+
 
 const routes: Routes = [
-  {path: 'departments', component: DepartmentListComponentComponent},
-  {path: 'employees', component: EmployeeListComponentComponent}
+  {path: '', redirectTo: '/departmentsList', pathMatch: 'full'},
+  {path: 'departmentsList', component: DepartmentListComponentComponent},
+  {path: 'employeesList', component: EmployeeListComponentComponent},
+  {path: 'departmentsList/:id', component: DepartmentDetailComponentComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -18,5 +25,7 @@ const routes: Routes = [
 
 })
 
-export class AppRoutingModule{}
-export const routingComponents=[DepartmentListComponentComponent,EmployeeListComponentComponent]
+export class AppRoutingModule {
+}
+
+export const routingComponents = [DepartmentListComponentComponent, EmployeeListComponentComponent, DepartmentDetailComponentComponent, HomeComponent, PageNotFoundComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {routingComponents} from './app-routing.module';
+// import { DepartmentDetailComponentComponent } from './components/department-detail-component/department-detail-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
